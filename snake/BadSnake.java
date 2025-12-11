4264ffb0858ce06acdf74c8812d88f40fb60abcb
fetrarison <hei.fetra.2@gmail.com>
2025-12-11 15:34:56 +0300

    feat refactoring snake game

--- snake/BadSnake.java
+++ snake/BadSnake.java
@@ -151,7 +151,7 @@ public class BadSnake {
                 }
                 sb.append("\n");
             }
-//
+
             System.out.print("\033[H\033[2J");
             System.out.flush();
             System.out.println(sb.toString());
